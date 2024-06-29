7accda17774ccec5a90f2b9cee1b6e391e6c6d09
syahrul <syahrul19112003@gmail.com>
2024-06-29 22:56:59 +0700

    update style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,17 +32,17 @@ const Navbar = () => {
         </Link>
         </div>
         <div className="hidden md:flex space-x-4 font-bold font-spaceGrotesk">
-          <Link to="/" className={`nav-link ${isSticky ? 'text-black' : 'text-white'}`}>Home</Link>
-          <Link to="/program" className={`nav-link ${isSticky ? 'text-black' : 'text-white'}`}>Program</Link>
-          <Link to="/author" className={`nav-link ${isSticky ? 'text-black' : 'text-white'}`}>Author</Link>
-          <Link to="/information" className={`nav-link ${isSticky ? 'text-black' : 'text-white'}`}>Information</Link>
+          <Link to="/" className={`nav-link ${isSticky ? 'text-white' : 'text-white'}`}>Home</Link>
+          <Link to="/program" className={`nav-link ${isSticky ? 'text-white' : 'text-white'}`}>Program</Link>
+          <Link to="/author" className={`nav-link ${isSticky ? 'text-white' : 'text-white'}`}>Author</Link>
+          <Link to="/information" className={`nav-link ${isSticky ? 'text-white' : 'text-white'}`}>Information</Link>
         </div>
         <div className="hidden md:flex space-x-4 font-bold">
-          <Link to="/login" className={`mt-2 nav-link ${isSticky ? 'text-black' : 'text-white'}`}>Login</Link>
-          <Link to="/signup" className={`nav-link ${isSticky ? 'bg-darkGreen text-white' : 'bg-darkGreen text-white'} px-4 py-2 rounded-full`}>Sign Up</Link>
+          <Link to="/login" className={`mt-2 nav-link ${isSticky ? 'text-white' : 'text-white'}`}>Login</Link>
+          <Link to="/signup" className={`nav-link ${isSticky ? 'bg-colorGreen text-black' : 'bg-colorGreen text-black'} px-4 py-2 rounded-full`}>Sign Up</Link>
         </div>
         <div className="md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} className={`nav-link ${isSticky ? 'text-black' : 'text-white'}`}>
+          <button onClick={() => setIsOpen(!isOpen)} className={`nav-link ${isSticky ? 'text-white' : 'text-white'}`}>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
             </svg>
@@ -50,13 +50,13 @@ const Navbar = () => {
         </div>
       </div>
       {isOpen && (
-        <div className={`md:hidden p-2 font-bold ${isSticky ? 'bg-gray-400 bg-opacity-50' : 'bg-green-900 font-bold text-white'}`}>
+        <div className={`md:hidden p-2 font-bold ${isSticky ? 'bg-green-900 font-bold text-white' : 'bg-green-900 font-bold text-white'}`}>
           <Link to="/" className="block nav-link py-2">Home</Link>
           <Link to="/program" className="block nav-link py-2">Program</Link>
           <Link to="/author" className="block nav-link py-2">Author</Link>
           <Link to="/information" className="block nav-link py-2">Information</Link>
           <Link to="/login" className="block nav-link py-2">Login</Link>
-          <Link to="/signup" className="block nav-link bg-green-500 px-4 py-2 rounded mt-2">Sign Up</Link>
+          <Link to="/signup" className="block nav-link bg-green-500 px-4 py-2 text-black rounded mt-2">Sign Up</Link>
         </div>
       )}
     </nav>
