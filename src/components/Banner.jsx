0db3567c289c2a53cb5546501e2e369220cfee34
syahrul <syahrul19112003@gmail.com>
2024-06-29 22:30:21 +0700

    update banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,8 +7,8 @@ const Banner = () => {
         <>
         <div className="bannerHome h-screen flex-row font-spaceGrotesk">
             <div className="font-bold mt-5">
-                <h1 className="md:text-5xl text-white">1st International Conference On Sustainability of Sciences <br />for the Future (ICSSF)</h1>
-                <h1 className="text-darkGreen">September 26-27th, 2024</h1>
+                <h1 className="md:text-5xl text-white">1st International Conference <br />On Sustainability of Sciences <br />for the Future ( ICSSF )</h1>
+                <h1 className="text-darkGreen pt-5">September 26-27th, 2024</h1>
                 <h1 className="text-xl text-white italic">"Building Up The Next Generation of Scientists"</h1>
                 <div className="mt-[50px] flex items-center justify-center">
                     <Link className="bg-darkGreen flex w-fit py-2 px-5 items-center rounded-full gap-1" to={'/'}><IconFileDescription className="w-5 mb-1"/> Call for Paper</Link>
