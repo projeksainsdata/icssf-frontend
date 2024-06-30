4b32151cf6557d498d6673cb51870ae9db2825d5
syahrul <syahrul19112003@gmail.com>
2024-06-30 09:51:11 +0700

    update responsive banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,13 +7,13 @@ const Banner = () => {
         <>
         <div className="bannerHome h-screen flex-row font-spaceGrotesk">
             <div className="font-bold mt-5">
-            <h1 class="text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-white text-center px-2">
+            <h1 class="text-2xl sm:text-2xl md:text-4xl lg:text-5xl text-white text-center mb-5">
                 1st International Conference <br />
                 On Sustainability of Sciences <br />
                 for the Future ( ICSSF )
             </h1>
-                <h1 className="text-colorGreen text-2xl sm:text-2xl md:text-3xl lg:text-3xl pt-5">September 26-27th, 2024</h1>
-                <h1 className="text-2xl sm:text-2xl md:text-2xl lg:text-2xl text-white italic">"Building Up The Next Generation of Scientists"</h1>
+                <h1 className="text-colorGreen text-xl sm:text-xl md:text-3xl lg:text-3xl pt-5">September 26-27th, 2024</h1>
+                <h1 className="text-xl sm:text-xl md:text-2xl lg:text-2xl text-white italic">"Building Up The Next Generation of Scientists"</h1>
                 <div className="mt-[50px] flex items-center justify-center">
                     <Link className="bg-colorGreen flex w-fit py-2 px-5 items-center rounded-full gap-1" to={'/'}><IconFileDescription className="w-5 mb-1"/> Call for Paper</Link>
                 </div>
