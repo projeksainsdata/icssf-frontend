3a4fe3d6d7f2f7084ab8c44ddfef6982b1ee896f
syahrul <syahrul19112003@gmail.com>
2024-06-30 19:02:59 +0700

    update keynote

--- src/components/Keynote.jsx
+++ src/components/Keynote.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ImageVanue from "./../assets/images/imageKeynot.png";
 
 const Keynote = () => {
     return (
@@ -6,26 +7,26 @@ const Keynote = () => {
             <div className="text-center font-plusJakarta mt-10">
                 <h1 className="text-white font-plusJakarta text-3xl font-bold">Keynote Speakers</h1>
 
-                <div className="mt-20 grid grid-cols-1 md:grid-cols-3 gap-10 md:px-[100px] px-10">
+                <div className="mt-20 grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10 md:px-[100px] px-10">
                     <div className="flex items-center mb-2 bg-key p-5 rounded-xl">
-                        <img src='https://placehold.co/700x700' className="w-[60px] rounded-full" />
+                        <img src={ImageVanue} className="w-[60px] rounded-full" />
                         <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Masgalih.net</p>
-                            <p className="text-gray-500 text-[10px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
+                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
                         </div>
                     </div>
                     <div className="flex items-center mb-2 bg-key p-5 rounded-xl">
-                        <img src='https://placehold.co/700x700' className="w-[60px] rounded-full" />
+                        <img src={ImageVanue} className="w-[60px] rounded-full" />
                         <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Masgalih.net</p>
-                            <p className="text-gray-500 text-[10px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
+                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
                         </div>
                     </div>
                     <div className="flex items-center mb-2 bg-key p-5 rounded-xl">
-                        <img src='https://placehold.co/700x700' className="w-[60px] rounded-full" />
+                        <img src={ImageVanue} className="w-[60px] rounded-full" />
                         <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Masgalih.net</p>
-                            <p className="text-gray-500 text-[10px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
+                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
                         </div>
                     </div>
                 </div>
