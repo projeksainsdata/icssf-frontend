a1a69522c15e45e6e73866125ee53d78c09b93d6
projeksainsdata <projeksainsdata@gmail.com>
2024-07-01 23:15:01 +0700

    invited speaker

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import ImageVanue from "./../assets/images/imageKeynot.png";
+import inv1 from "./../assets/images/hakim.png";
+import inv2 from "./../assets/images/azrul.jpg";
+import inv3 from "./../assets/images/werry.jpg";
+import inv4 from "./../assets/images/anjar.jpg";
+import inv5 from "./../assets/images/robi.jpg";
 
 const Speaker = () => {
     return (
@@ -8,46 +12,39 @@ const Speaker = () => {
                 <h1 className="text-white font-plusJakarta text-3xl font-bold">Invited Speakers</h1>
 
                 <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10 md:px-[100px] px-10">
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                    <div className="bg-black p-5 rounded-xl flex flex-col items-center">
+                        <img src={inv1} className="w-40 h-40 object-cover rounded-lg mb-3" />
+                        <div className="text-center">
+                            <p className="text-xl text-colorGreen font-bold">Dr. Hakim L. Malasan</p>
+                            <p className="text-white text-base mt-2">Department of Astronomy, Institut Teknologi Bandung, Indonesia  </p>
                         </div>
                     </div>
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                    <div className="bg-black p-5 rounded-xl flex flex-col items-center">
+                        <img src={inv2} className="w-40 h-40 object-cover rounded-lg mb-3" />
+                        <div className="text-center">
+                            <p className="text-xl text-colorGreen font-bold">Azrul S. K. Pohan, PhD</p>
+                            <p className="text-white text-base mt-2">Department of Physics, Institut Teknologi Sumatera, Indonesia</p>
                         </div>
                     </div>
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                    <div className="bg-black p-5 rounded-xl flex flex-col items-center">
+                        <img src={inv3} className="w-40 h-40 object-cover rounded-lg mb-3" />
+                        <div className="text-center">
+                            <p className="text-xl text-colorGreen font-bold">Dr. Werry Febrianti</p>
+                            <p className="text-white text-base mt-2">Department of Mathematics, Institut Teknologi Sumatera, Indonesia  </p>
                         </div>
                     </div>
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                    <div className="bg-black p-5 rounded-xl flex flex-col items-center">
+                        <img src={inv4} className="w-40 h-40 object-cover rounded-lg mb-3" />
+                        <div className="text-center">
+                            <p className="text-xl text-colorGreen font-bold">Dr. apt. Anjar Hermadi Saputro, M.Pharm.Sci</p>
+                            <p className="text-white text-base mt-2">Department of Pharmacy, Institut Teknologi Sumatera, Indonesia  </p>
                         </div>
                     </div>
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
-                        </div>
-                    </div>
-                    <div className="flex items-center mb-2 bg-black p-5 rounded-xl">
-                        <img src={ImageVanue} className="w-[60px] rounded-full" />
-                        <div className="flex flex-col ml-3">
-                            <p className="text-sm text-colorGreen font-bold text-left">Prof. Hideyo Kawakita</p>
-                            <p className="text-white text-[11px] mt-2 text-left">Faculty of Science, Department of Astrophysics and Meteorology, Kyoto Sangyo University</p>
+                    <div className="bg-black p-5 rounded-xl flex flex-col items-center">
+                        <img src={inv5} className="w-40 h-40 object-cover rounded-lg mb-3" />
+                        <div className="text-center">
+                            <p className="text-xl text-colorGreen font-bold">Dr. Robiatul Muztaba</p>
+                            <p className="text-white text-base mt-2">Department of Astronomy, Institut Teknologi Sumatera, Indonesia</p>
                         </div>
                     </div>
                 </div>
@@ -56,4 +53,4 @@ const Speaker = () => {
     )
 }
 
-export default Speaker
+export default Speaker;
--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,62 +1,100 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { IconSchool,IconChevronDown  } from '@tabler/icons-react';
-import scientist from '../assets/images/scientist.png'
+import React, { useState } from "react";
+import { IconSchool, IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import scientist from '../assets/images/scientist.png';
+
 const Topics = () => {
+    const [openTopic, setOpenTopic] = useState("Nature Sciences");
+
+    const toggleTopic = (name) => {
+        setOpenTopic(openTopic === name ? null : name);
+    };
+
+    const topics = [
+        {
+            name: "Nature Sciences",
+            details: [
+                "Mathematics",
+                "Physics",
+                "Biology",
+                "Chemistry",
+                "Astronomy",
+                "Meteorology",
+                "Atmospheric and Planetary Science",
+                "Earth Science"
+            ]
+        },
+        {
+            name: "Enivonmental Sciences",
+            details: [
+                "Global Environmental Change and Disaster Management",
+                "Natural Resources Science",
+                "Environmental Dynamics and Ecosystem",
+                "Environmental Management and Policy",
+                "Renewable energy",
+                "Ecology and Biodiversity",
+                "Enviromental Pollution",
+                "Ocean and coastal",
+                "Solid Waste Management",
+                "Global Pandemic",
+                "Carbon Footprint",
+                "Urban city heat impac"                
+            ]
+        },
+        {
+            name: "Pharmacy",
+            details: [
+                "Pharmaceutical Sciences",
+                "Global Health and Pharmacy",
+                "Pharmacy Automation and Technology",
+                "Physical Pharmacy and Cosmetic",
+                "Natural Products Chemistry and Pharmacognosy",
+                "Medicinal and Pharmaceutical Chemistry",
+                "Precision Medicine",
+                "Pharmacy Policy and Regulatory",
+                "Clinical and Industrial Drug"
+                
+            ]
+        }
+    ];
+
     return (
         <>
-        {/* Conference Topics */}
-
-        <div className="flex flex-col md:flex-row gap-10 m-5 my-10">
-            <div className="text-white font-spaceGrotesk space-y-10">
-                <div className="flex flex-col gap-3">
-                    <h3>1st International Conference On Sustainability of Sciences for the Future (ICSSF)</h3>
-                    <h1 className="text-3xl md:text-4xl font-bold text-colorGreen">Conference Topics</h1>
-                </div>
-                <div className="flex flex-col gap-3">
-                {["Enivonmental Sciences","Pharmacy"].map(name=>
-                    <div className="px-5 py-3 bg-[#121212] font-plusJakarta hover:bg-colorGreen hover:text-black cursor-pointer rounded-lg space-y-5">
-                        <div className="flex justify-between">
-                            <div className="flex items-center gap-3">
-                                <IconSchool className="text-colorGreen"/>
-                                <h3 className="text-lg font-semibold">{name}</h3>
+            {/* Conference Topics */}
+            <div className="flex flex-col md:flex-row gap-10 m-5 my-10">
+                <div className="text-white font-spaceGrotesk space-y-10">
+                    <div className="flex flex-col gap-3">
+                        <h3>1st International Conference On Sustainability of Sciences for the Future (ICSSF)</h3>
+                        <h1 className="text-3xl md:text-4xl font-bold text-colorGreen">Conference Topics</h1>
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        {topics.map((topic, index) => (
+                            <div key={index} className="px-5 py-3 bg-[#121212] font-plusJakarta hover:bg-colorGreen hover:text-black cursor-pointer rounded-lg space-y-5">
+                                <div className="flex justify-between" onClick={() => toggleTopic(index)}>
+                                    <div className="flex items-center gap-3">
+                                        <IconSchool className="text-white"/>
+                                        <h3 className="text-lg font-semibold">{topic.name}</h3>
+                                    </div>
+                                    {openTopic === index ? <IconChevronUp /> : <IconChevronDown />}
+                                </div>
+                                {openTopic === index && (
+                                    <div>
+                                        <ul className="list-disc mx-5 space-y-1">
+                                            {topic.details.map((detail, i) => (
+                                                <li key={i}>{detail}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                )}
                             </div>
-                            <IconChevronDown/>
-                        </div>
-                    </div>
-                )}
-                <div className="px-5 py-3 bg-[#121212] font-plusJakarta hover:bg-colorGreen hover:text-black cursor-pointer rounded-lg space-y-5">
-                    <div className="flex justify-between">
-                        <div className="flex items-center gap-3">
-                            <IconSchool className="text-colorGreen hover:text-black"/>
-                            <h3 className="text-lg font-semibold">Nature Sciences</h3>
-                        </div>
-                        <IconChevronDown/>
-                    </div>
-                    <div>
-                    <ul className="list-disc mx-5 space-y-1">
-                        <li>Global Environmental Change and Disaster Management</li>
-                        <li>Natural Resources Science</li>
-                        <li>Environmental Dynamics and Ecosystem</li>
-                        <li>Environmental Management and Policy</li>
-                        <li>Renewable energy</li>
-                        <li>Ecology and Biodiversity</li>
-                        <li>Environmental Pollution</li>
-                        <li>Solid Waste Management</li>
-                        <li>Global Pandemic</li>
-                        <li>Carbon Footprint</li>
-                        <li>Urban city heat impact</li>
-                    </ul>
+                        ))}
                     </div>
                 </div>
+                <div className="md:order-1 order-first">
+                    <img src={scientist} className="rounded-lg md:rounded-l-lg" />
                 </div>
             </div>
-            <div className="md:order-1 order-first">
-                <img src={scientist} className="rounded-lg md:rounded-l-lg"/>
-            </div>
-        </div>
         </>
-    )
-}
+    );
+};
 
-export default Topics
+export default Topics;
